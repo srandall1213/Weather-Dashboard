a595e3daf28ad3831f365daf957340a5bdb93bc0
Randie <srandall1213@gmail.com>
2022-07-15 12:41:16 -0600

    forloop not working on fetch - getting help

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,27 +1,38 @@
 var apiKey = '044bb9c5369619c2020f969f5078b5a5';
 var cityInput = document.querySelector('#cityInput');
 var searchBtn = document.querySelector('#searchBtn');
+var todayContainerEl = document.querySelector('#todayContainer')
 
 function todayBox() {
-    var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityInput.value + '&appid=' + apiKey;
+    var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityInput.value + '&appid=' + apiKey + '&units=imperial';
     fetch(queryURL)
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
         console.log(data);
-        // for (var i = 0; i < data.length; i++) {
-        //     $("#city").text(cityInput);
+        for (var i = 0; i < data.length; i++) {
+        console.log(data[i]);
+            
+            var cityName = document.createElement('h3');
+            cityName.textContent = data[i].name;
+            todayContainerEl.append(cityName);
 
-        //     var todayDateEl = moment();
-        //     $('#todayDate').text(todayDateEl.format("(MM/DD/YYYY)"));
+            // $("#city").text(data[i].name);
 
-        //     $("#tempToday").text(data[i].temperature);
-        //     $("#windToday").text(data[i].wind);
-        //     $("#humidityToday").text(data[i].humidity);
-        //     $("#uvIndexToday").text(data[i].uvindex);
+        
+            // var todayDateEl = moment();
+            // $('#todayDate').text(todayDateEl.format("(MM/DD/YYYY)"));
 
-        // }
+            //$("#todayEmoji").text(data[i].weather.0.icon)
+            
+
+            // $("#tempToday").text(data[i].main.temp);
+            // $("#windToday").text(data[i].wind.speed);
+            // $("#humidityToday").text(data[i].main.humidity);
+            // $("#uvIndexToday").text(data[i].);
+
+        }
       });
-  }
+}
 searchBtn.addEventListener('click', todayBox);
