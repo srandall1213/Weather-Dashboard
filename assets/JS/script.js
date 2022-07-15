014641ab4e76b029bec58d3f560456010fa07ad5
Randie <srandall1213@gmail.com>
2022-07-15 10:47:12 -0600

    started fetch-not working-getting help

---
+++ assets/JS/script.js
@@ -0,0 +1,28 @@
+var apiKey = '044bb9c5369619c2020f969f5078b5a5';
+var cityInput = document.querySelector('#cityInput');
+var searchBtn = document.querySelector('#searchBtn');
+
+function todayBox() {
+    var queryURL = 'api.openweathermap.org/data/2.5/weather?q=' + cityInput + '&appid=' + apiKey;
+    fetch(queryURL)
+      .then(function (response) {
+        console.log(response)
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        // for (var i = 0; i < data.length; i++) {
+        //     $("#city").text(cityInput);
+
+        //     var todayDateEl = moment();
+        //     $('#todayDate').text(todayDateEl.format("(MM/DD/YYYY)"));
+
+        //     $("#tempToday").text(data[i].temperature);
+        //     $("#windToday").text(data[i].wind);
+        //     $("#humidityToday").text(data[i].humidity);
+        //     $("#uvIndexToday").text(data[i].uvindex);
+
+        // }
+      });
+  }
+searchBtn.addEventListener('click', todayBox);
